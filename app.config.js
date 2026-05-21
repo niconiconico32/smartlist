349c3a87db444e12c82511da7367fb9c10dbcfc4
nico <nico@MacBook-Air-de-nico.local>
2026-05-21 01:02:18 -0400

    chore: upgrade to Expo SDK 55 + configure EAS build image sdk-55

--- app.config.js
+++ app.config.js
@@ -5,4 +5,5 @@ module.exports = ({ config }) => ({
     posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
     posthogHost: process.env.POSTHOG_HOST,
   },
+  plugins: ["expo-widgets", "expo-localization", "@react-native-community/datetimepicker"],
 });
