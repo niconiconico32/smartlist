55ba4e08b3406a864832fa035961b24d11c0ca8e
nico <nico@MacBook-Air-de-nico.local>
2026-05-21 01:31:39 -0400

    fix: exclude ios/ from EAS upload + fix app.config.js plugins override

--- app.config.js
+++ app.config.js
@@ -5,5 +5,9 @@ module.exports = ({ config }) => ({
     posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
     posthogHost: process.env.POSTHOG_HOST,
   },
-  plugins: ["expo-widgets", "expo-localization", "@react-native-community/datetimepicker"],
+  plugins: [
+    ...(config.plugins || []),
+    "expo-localization",
+    "@react-native-community/datetimepicker",
+  ],
 });
